20a5cfcc74b9109befc28e8d2e9938c60eedaf10
Mohamed Sherif Abdel Aziz <m7sherif@hotmail.com>
2022-09-18 12:18:52 +0200

    Update main.go

--- main.go
+++ main.go
@@ -10,9 +10,19 @@ var port = flag.Int("p", 8080, "server port")
 func main() {
 	flag.Parse()
 	http.HandleFunc("/", HelloServer)
+	http.HandleFunc("/mo", HelloServer1)
+	http.HandleFunc("/sherr", HelloServer2)
 	http.ListenAndServe(fmt.Sprintf("0.0.0.0:%d", *port), nil)
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+}
+
+func HelloServer1(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, Mo %s!", r.URL.Path[1:])
+}
+
+func HelloServer2(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, Sherr %s!", r.URL.Path[1:])
 }
